fbc5ca96a404d19624e72e79398447c47d22c3c5
Daria073 <56068378+Daria073@users.noreply.github.com>
2021-06-02 16:25:29 +0300

    Add files via upload

--- server.R
+++ server.R
@@ -6,30 +6,10 @@ library("pracma")
 library("miscTools")
 library("klaR")
 library("ggpubr")
+library("e1071")
+library("caret")
 library(shiny)
 
-yadr1 <- function(x,n,m){
-  FF <- matrix(data=NA, nrow = n, ncol = m)
-  for (j in 1:m) {
-    for (i in 1:n){
-      FF[i,j] <- as.vector(t(x[i,]))%*%as.numeric(x[j,])
-    }
-  }
-  return(FF)
-}
-yadr2 <- function(x,n,m){
-  p <- 1
-  # p <- 2
-  FF <- matrix(data=NA, nrow = n, ncol = m)
-  E <- matrix(data=0, nrow = n, ncol = m)
-  E <- diag(rep(1,n))
-  for (j in 1:m) {
-    for (i in 1:n){
-      FF[i,j] <- (E[i,j] + as.vector(t(x[i,]))%*%as.numeric(x[j,]))^p
-    }
-  }
-  return(FF)
-}
 yadr3 <- function(x,n,m){
   gamma <- 1
   FF <- matrix(data=NA, nrow = n, ncol = m)
@@ -121,56 +101,47 @@ vtvertrain <- function(wmp,FF,m){
   return(t.itog)
 }
 
+methodsvm <- function(rak1,t){
+  svm1 <- svm(formula = t ~ V3+V4+V5+V6+V7+V8+V9+V10+V11+V12, data = rak1, 
+      cross = 10, kernel = "radial", gamma = 0.5, cost = 4, type = "C-classification")
+  return(svm1)
+}
+
 rak <- read.table("data/rak.data", head = FALSE, as.is = TRUE, sep =',')
 rak1 <- rak
 rak <- rak[3:12]
-ill<-read.table("data/index.csv", head=TRUE, sep = ";")
-ill <- na.omit(ill)
-ill1 <- ill
-ill <- ill[2:10]
 
 shinyServer(function(input, output) {
   
   datasetInput <- reactive({
-    switch(input$dataset,
-           "rak" = rak,
-           "ill" = ill)
+    rak
   })
   NormaInput <- reactive({
     switch(input$normalise, "normalise" = {data.frame(scale(datasetInput()))})
   })
   nInput <- reactive({ nrow(datasetInput()[input$obv[1]:input$obv[2],]) })
   mInput <- reactive({ nrow(datasetInput()[input$obv[1]:input$obv[2],]) })
+  
   yaderInput <- reactive({
-    switch(input$yader,
-           "Linear_core" = yadr1(NormaInput()[input$obv[1]:input$obv[2],],nInput(),mInput()),
-           "Polynomial_core" = yadr2(NormaInput()[input$obv[1]:input$obv[2],],nInput(),mInput()),
-           "Gaussian_core" = yadr3(NormaInput()[input$obv[1]:input$obv[2],],nInput(),mInput()))
+    yadr3(NormaInput()[input$obv[1]:input$obv[2],],nInput(),mInput())
   })
   tInput <- reactive({ 
-    if (input$dataset == "rak") {
     rak1$V2[input$obv[1]:input$obv[2]]
-  } else {
-    ill1$X11[input$obv[1]:input$obv[2]]
-  }
     })
   verInput <- reactive({
     NormaInput()[-(input$obv[1]:input$obv[2]),]
   })
   tverInput <- reactive ({
-    if (input$dataset == "rak") {
       rak1$V2[-(input$obv[1]:input$obv[2])]
-    } else {
-      ill1$X11[-(input$obv[1]:input$obv[2])]
-    }
   })
   yaderverInput <- reactive({
-    switch(input$yader,
-           "Linear_core" = yadr1(verInput(),nInput(),length(tverInput())),
-           "Polynomial_core" = yadr2(verInput(),nInput(),length(tverInput())),
-           "Gaussian_core" = yadr3(verInput(),nInput(),length(tverInput())))
+      yadr3(verInput(),nInput(),length(tverInput()))
   })
 
+  methodInput <- reactive({
+    methodsvm(NormaInput()[input$obv[1]:input$obv[2],],tInput())
+  })
+  
   output$view <- renderTable({
     if(input$normalise == TRUE){
       head(NormaInput()[input$obv[1]:input$obv[2],])
@@ -182,52 +153,30 @@ shinyServer(function(input, output) {
     paste("Данная выборка имеет ", nrow(datasetInput()), " наблюдений.")
   })
   
-  output$plot1 <- renderPlot({
-    #if (input$result == TRUE){
-    if(input$normalise == TRUE){
-    wmp <- train(NormaInput()[input$obv[1]:input$obv[2],],yaderInput(),tInput())
-    pp <- data.frame(t = tInput(),x = NormaInput()[input$obv[1]:input$obv[2],],wmp = wplot(wmp,mInput()))
-    if (input$dataset == "rak") {
-      g1 <- ggplot(data = pp, aes(x = rak$V3[input$obv[1]:input$obv[2]],
-                                  y = rak$V4[input$obv[1]:input$obv[2]],
-                                  shape = factor(t), color = factor(t), size = 0.02)) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
-      g3 <- ggplot(data = pp, aes(x = rak$V3[input$obv[1]:input$obv[2]],
-                                  y = rak$V4[input$obv[1]:input$obv[2]],
-                                  shape = factor(wmp), color = factor(wmp), size = 0.08)) + geom_point(color = "green")+ xlab ("Радиус") + ylab ("Текстура")
-      ggarrange(g1,g3)
-    } else {
-      g1 <- ggplot(data = pp, aes(x = ill$X5[input$obv[1]:input$obv[2]],
-                                  y = ill$X8[input$obv[1]:input$obv[2]],
-                                  shape = factor(t), color = factor(t), size = 0.02)) + geom_point()+ xlab ("ALT") + ylab ("Общий белок")
-      g3 <- ggplot(data = pp, aes(x = ill$X5[input$obv[1]:input$obv[2]],
-                                  y = ill$X8[input$obv[1]:input$obv[2]],
-                                  shape = factor(wmp), color = factor(wmp), size = 0.08)) + geom_point(color = "green")+ xlab ("ALT") + ylab ("Общий белок")
-      ggarrange(g1,g3)
-   # }
-    } }
-  })  
   output$plot2 <- renderPlot({
    # if (input$result == TRUE){
     if(input$normalise == TRUE){
     wmp <- train(NormaInput()[input$obv[1]:input$obv[2],],yaderInput(),tInput())
     pp <- data.frame(t = tInput(),x = NormaInput()[input$obv[1]:input$obv[2],],t.tr = vttrain(wmp,yaderInput(),mInput()))
-    if (input$dataset == "rak") {
-        g1 <- ggplot(data = pp, aes(x = rak$V3[input$obv[1]:input$obv[2]],
-                                    y = rak$V4[input$obv[1]:input$obv[2]],
-                                    shape = factor(t), color = factor(t), size = 0.02)) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
-        g3 <- ggplot(data = pp, aes(x = rak$V3[input$obv[1]:input$obv[2]],
-                                    y = rak$V4[input$obv[1]:input$obv[2]],
-                                    shape = factor(t.tr), color = factor(t.tr), size = 0.04)) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
-        ggarrange(g1,g3)
-      } else {
-        g1 <- ggplot(data = pp, aes(x = ill$X5[input$obv[1]:input$obv[2]],
-                                    y = ill$X8[input$obv[1]:input$obv[2]],
-                                    shape = factor(t), color = factor(t), size = 0.02)) + geom_point()+ xlab ("ALT") + ylab ("Общий белок")
-        g3 <- ggplot(data = pp, aes(x = ill$X5[input$obv[1]:input$obv[2]],
-                                    y = ill$X8[input$obv[1]:input$obv[2]],
-                                    shape = factor(t.tr), color = factor(t.tr), size = 0.04)) + geom_point()+ xlab ("ALT") + ylab ("Общий белок")
-        ggarrange(g1,g3)
-    #  }
+    pp2 <- data.frame(x = NormaInput()[input$obv[1]:input$obv[2],],t = tInput(),s = methodInput()$fitted)
+    if (input$method == "RVM") {
+      g1 <- ggplot(data = pp, aes(x = rak$V3[input$obv[1]:input$obv[2]],
+                                  y = rak$V4[input$obv[1]:input$obv[2]],
+                                  shape = factor(t), color = factor(t))) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
+      g3 <- ggplot(data = pp, aes(x = rak$V3[input$obv[1]:input$obv[2]],
+                                  y = rak$V4[input$obv[1]:input$obv[2]],
+                                  shape = factor(t.tr), color = factor(t.tr))) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
+      ggarrange(g1,g3)
+    } else {
+      
+      g1 <- ggplot(data = pp2, aes(x = rak$V3[input$obv[1]:input$obv[2]],
+                                   y = rak$V4[input$obv[1]:input$obv[2]],
+                                   shape = factor(t), color = factor(t))) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
+      g3 <- ggplot(data = pp2, aes(x = rak$V3[input$obv[1]:input$obv[2]],
+                                   y = rak$V4[input$obv[1]:input$obv[2]],
+                                   shape = factor(s), color = factor(s))) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
+      ggarrange(g1,g3)
+      # }
     } }
   })
   output$plot3 <- renderPlot({
@@ -235,45 +184,60 @@ shinyServer(function(input, output) {
     if(input$normalise == TRUE){
     wmp <- train(NormaInput()[input$obv[1]:input$obv[2],],yaderInput(),tInput())
     pp <- data.frame(t = tverInput(),x = verInput(),t.itog = vtvertrain(wmp,yaderverInput(),length(tverInput())))
-     if (input$dataset == "rak") {
-        g1 <- ggplot(data = pp, aes(x = rak$V3[-(input$obv[1]:input$obv[2])],
-                                    y = rak$V4[-(input$obv[1]:input$obv[2])],
-                                    shape = factor(t), color = factor(t))) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
-        g3 <- ggplot(data = pp, aes(x = rak$V3[-(input$obv[1]:input$obv[2])],
-                                    y = rak$V4[-(input$obv[1]:input$obv[2])],
-                                    shape = factor(t.itog), color = factor(t.itog))) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
-        ggarrange(g1,g3)
-      } else {
-        g1 <- ggplot(data = pp, aes(x = ill$X5[-(input$obv[1]:input$obv[2])],
-                                    y = ill$X8[-(input$obv[1]:input$obv[2])],
-                                    shape = factor(t), color = factor(t))) + geom_point()+ xlab ("ALT") + ylab ("Общий белок")
-        g3 <- ggplot(data = pp, aes(x = ill$X5[-(input$obv[1]:input$obv[2])],
-                                    y = ill$X8[-(input$obv[1]:input$obv[2])],
-                                    shape = factor(t.itog), color = factor(t.itog))) + geom_point()+ xlab ("ALT") + ylab ("Общий белок")
-        ggarrange(g1,g3)
-    #  }
-    } 
-    }
+    pp2 <- data.frame(x = verInput(),t = tverInput(),s.itog = predict(methodInput(), verInput()))
+    if (input$method == "RVM") {
+      g1 <- ggplot(data = pp, aes(x = rak$V3[input$obv[1]:input$obv[2]],
+                                  y = rak$V4[input$obv[1]:input$obv[2]],
+                                  shape = factor(t), color = factor(t))) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
+      g3 <- ggplot(data = pp, aes(x = rak$V3[input$obv[1]:input$obv[2]],
+                                  y = rak$V4[input$obv[1]:input$obv[2]],
+                                  shape = factor(t.itog), color = factor(t.itog))) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
+      ggarrange(g1,g3)
+    } else {
+      
+      g1 <- ggplot(data = pp2, aes(x = rak$V3[input$obv[1]:input$obv[2]],
+                                   y = rak$V4[input$obv[1]:input$obv[2]],
+                                   shape = factor(t), color = factor(t))) + geom_point()+ xlab ("Радиус") + ylab ("Текстура")
+      g3 <- ggplot(data = pp2, aes(x = rak$V3[input$obv[1]:input$obv[2]],
+                                   y = rak$V4[input$obv[1]:input$obv[2]],
+                                   shape = factor(s.itog), color = factor(s.itog))) + geom_point() + xlab ("Радиус") + ylab ("Текстура")
+      ggarrange(g1,g3)
+      # }
+    } } 
   })
   
   output$summary1 <- renderTable({
    # if (input$result == TRUE){
-    wmp <- train(NormaInput()[input$obv[1]:input$obv[2],],yaderInput(),tInput())
-    Acc0 <- mean(vttrain(wmp,yaderInput(),mInput()) == tInput())
+    if (input$method == "RVM") {
+      wmp <- train(NormaInput()[input$obv[1]:input$obv[2],],yaderInput(),tInput())
+      Acc0 <- mean(vttrain(wmp,yaderInput(),mInput()) == tInput())
+    } else {
+      Acc0 <- mean(methodInput()$fitted == tInput())
+      # }
+    } 
     paste("Точность=",round(100*Acc0,2),"%")
    # }
   })
   output$summary2 <- renderTable({
    # if (input$result == TRUE){
-    wmp <- train(NormaInput()[input$obv[1]:input$obv[2],],yaderInput(),tInput())
-    (table(Факт = tverInput(), Прогноз = vtvertrain(wmp,yaderverInput(),length(tverInput()))))
-    #}
+    if (input$method == "RVM") {
+      wmp <- train(NormaInput()[input$obv[1]:input$obv[2],],yaderInput(),tInput())
+      (table(Факт = tverInput(), Прогноз = vtvertrain(wmp,yaderverInput(),length(tverInput()))))
+    } else {
+      (table(Факт = tverInput(), Прогноз = predict(methodInput(), verInput())))
+      # }
+    } 
   })
   
   output$summary3 <- renderTable({
    # if (input$result == TRUE){
-    wmp <- train(NormaInput()[input$obv[1]:input$obv[2],],yaderInput(),tInput())
-    Acc1 <- mean(vtvertrain(wmp,yaderverInput(),length(tverInput())) == tverInput())
+    if (input$method == "RVM") {
+      wmp <- train(NormaInput()[input$obv[1]:input$obv[2],],yaderInput(),tInput())
+      Acc1 <- mean(vtvertrain(wmp,yaderverInput(),length(tverInput())) == tverInput())
+    } else {
+      Acc1 <- mean(predict(methodInput(), verInput()) == tverInput())
+      # }
+    } 
     paste("Точность=",round(100*Acc1,2),"%")
    # }
   })
--- ui.R
+++ ui.R
@@ -1,36 +1,34 @@
 library(shiny)
 library(shinythemes)
-setwd("C:/mydipl")
+setwd("C:/pole")
 rak <- read.table("data/rak.data", head = FALSE, as.is = TRUE, sep =',')
-ill<-read.table("data/index.csv", head=TRUE, sep = ";")
 
 shinyUI(fluidPage( theme = shinytheme("united"),
-  titlePanel("Relevance Vector Machine"),
+  titlePanel("Программная система по диагностированию рака молочной железы"),
   
   sidebarLayout(
     sidebarPanel(
-      selectInput("dataset", "Выбери набор данных:",
-                  choices = c("rak", "ill")),
-      tags$hr(),
+      #selectInput("dataset", "Набор данных:",
+      #            choices = c("rak")),
+      #tags$hr(),
       checkboxInput("normalise", label = "Нормализовать данные", value = FALSE),
       sliderInput("obv", label = "Обучающая выборка:",
-                  min = 1, max = 600, value = c(1, 150)),
+                  min = 1, max = 600, value = c(1, 400)),
       
       helpText("Замечание: обучающая выборка не должна превышать всю выборку."),
-      selectInput("yader", label = "Выбери ядровую функцию:", 
-                  choices = c("Linear_core", "Polynomial_core",
-                                 "Gaussian_core"),
-                  selected = "Gaussian_core")
-     # actionButton("result", "Результат")
+      
+      selectInput("method", label = "Выбери метод:", 
+                  choices = c("SVM", "RVM"),
+                  selected = "RVM"),
+      actionButton("result", "Результат")
       ),
 
     mainPanel(
       tabsetPanel(type = "tabs",
                    tabPanel("Table", h4("Набор данных:"),tableOutput("view"),
                           textOutput("tr")),
-                  tabPanel("Plot", h4("Диаграммы рассеяния классов и релевантных векторов в проекциях на факторы по обучающей выборке:"),plotOutput("plot1"),
-                           h4("Диаграммы рассеяния классов исходной выборки и после обучения выборки:"),plotOutput("plot2"),
-                           h4("Сравнение результатов классификации RVM с исходными классами для тестовой выборки:"),plotOutput("plot3")),
+                  tabPanel("Plot", h4("Диаграммы рассеяния классов исходной выборки и после обучения выборки:"),plotOutput("plot2"),
+                          h4("Сравнение результатов классификации метода с исходными классами для тестовой выборки:"),plotOutput("plot3")),
                   tabPanel("Summary", h4("Оценка точности для обучающей выборки:"),tableOutput("summary1"),
                            h4("Сравнительная таблица количества фактической и прогнозируемой переменной:"),tableOutput("summary2"),
                            h4("Оценка точности классификации:"),tableOutput("summary3"))
